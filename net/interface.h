5ee025ea47f51d76d1963aec4bc1d93a2c893bb1
OryginalnaNazwa <szymon.mackowiak24@gmail.com>
2026-05-21 09:08:48 +0200

    New structure for the net, saving and loading
    - Saving weights and biases of the neurons to a file
    - Loading them from the same file
    - Padding added
    - Save/load controlled by preprocessor directives
    - Architecture prepared for full, working use

---
+++ net/interface.h
@@ -0,0 +1,17 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "net.h"
+
+#define TRAIN
+#define LOAD
+//#define FULL_EPOCH
+#define SAVE
+
+void NetRun();
+
+void NetTrain();
+
+void NetInterface();
+
+#endif // INTERFACE_H
